27908ed14a8e7ba6c6a46b16db5531f31359781a
Jay <jzawadzki19@gmail.com>
2020-08-18 01:31:50 +0200

    add search by author name

--- bookstore-spring/src/main/java/pl/zawadzki/bookstore/repository/AuthorRepository.java
+++ bookstore-spring/src/main/java/pl/zawadzki/bookstore/repository/AuthorRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.zawadzki.bookstore.model.Author;
 
+import java.util.Optional;
+
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Optional<Author> getByName(String name);
 }
