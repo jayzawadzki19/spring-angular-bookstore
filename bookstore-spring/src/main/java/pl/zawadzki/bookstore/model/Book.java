2007fe12e4ceb6f9b901bcbf656078da426361d9
Jay <jzawadzki19@gmail.com>
2020-10-02 23:04:16 +0200

    modify Book class

--- bookstore-spring/src/main/java/pl/zawadzki/bookstore/model/Book.java
+++ bookstore-spring/src/main/java/pl/zawadzki/bookstore/model/Book.java
@@ -1,30 +1,42 @@
 package pl.zawadzki.bookstore.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Data
 @Entity
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "books")
+@DynamicUpdate
+@NoArgsConstructor
 public class Book implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bookId;
+
     @NotBlank(message = "Title is required")
     private String title;
 
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @NotNull
     private double price;
+
+    /** Nuber of books in storage */
+    @NotNull
+    @Min(0)
+    private Integer bookStock;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "authorId", referencedColumnName = "authorId")
