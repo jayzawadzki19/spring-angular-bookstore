653ffd70db4112fbe012f1e1571aa15faab71900
Jay <jzawadzki19@gmail.com>
2020-11-11 19:01:52 +0100

    created http interceptor serv

---
+++ bookstore-angular/bookstore/src/app/auth/shared/http-interceptor.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {AuthService} from "./auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpInterceptorService implements HttpInterceptor {
+
+  constructor(private authService: AuthService) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const authToken = `Basic ${window.btoa(
+      this.authService.loginRequest.username + ":" + this.authService.loginRequest.password)}`
+
+    const authReq = req.clone({
+      headers: req.headers.set('authorization', authToken)
+    });
+    return next.handle(authReq);
+  }
+}
