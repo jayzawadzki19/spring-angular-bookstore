30b356b0bfeab08f40c3dd62ad1f287c2b6e9190
Jay <jzawadzki19@gmail.com>
2020-11-23 02:20:44 +0100

    created test for RegisterService

---
+++ bookstore-spring/src/test/java/pl/zawadzki/bookstore/service/RegisterServiceTest.java
@@ -0,0 +1,43 @@
+package pl.zawadzki.bookstore.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import pl.zawadzki.bookstore.dto.RegisterRequest;
+import pl.zawadzki.bookstore.model.User;
+import pl.zawadzki.bookstore.repository.CartRepository;
+import pl.zawadzki.bookstore.repository.UserRepository;
+
+@ExtendWith(MockitoExtension.class)
+class RegisterServiceTest {
+
+    @Mock
+    private UserRepository repository;
+    @Mock
+    private CartRepository cartRepository;
+    @Mock
+    private PasswordEncoder encoder;
+
+
+    @InjectMocks
+    private RegisterService service;
+
+    @Test
+    public void shouldRegisterUser() {
+        final User user =
+                User.builder()
+                        .username("testUser")
+                        .password(null)
+                        .email("testMail@qwe.com")
+                        .isActive(true)
+                        .role("ROLE_USER")
+                        .build();
+        service.signUp(new RegisterRequest(user.getEmail(),user.getUsername(),user.getPassword()));
+        Mockito.verify(repository,Mockito.times(1)).save(user);
+    }
+
+}
